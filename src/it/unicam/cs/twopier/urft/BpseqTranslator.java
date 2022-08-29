b6f081fdc0c845c7b6e0304fd68a84686a114a5c
Piero <68665714+xPieroxk@users.noreply.github.com>
2022-08-29 16:18:01 +0200

    implemented translation from BPSEQ to DB

--- src/it/unicam/cs/twopier/urft/BpseqTranslator.java
+++ src/it/unicam/cs/twopier/urft/BpseqTranslator.java
@@ -1,40 +1,59 @@
 package it.unicam.cs.twopier.urft;
 
+import java.util.ArrayList;
+
 public class BpseqTranslator implements RNAFormatTranslator {
 
 
     @Override
-    public String translateToDB(RNASecondaryStructure rnaSecondaryStructure) {
-        return "";
+    public FormattedRNAFile translateToDB(RNAFile rnaFile) {
+        if (rnaFile.formatType() != RNAFormatType.BPSEQ)
+            throw new IllegalArgumentException();
+        // add '# before each line of the header'
+        var header = new ArrayList<>(rnaFile.header().stream().map(l -> "#" + l).toList());
+        var body = new ArrayList<String>();
+        // fill the body
+        body.add(rnaFile.structure().getStructure());
+        body.add(rnaFile.structure().getSequence());
+        // create and return the formatted file as object
+        return new FormattedRNAFile(header,body);
     }
 
     @Override
-    public String translateToDBNoSequence(RNASecondaryStructure rnaSecondaryStructure) {
-        return "";
+    public FormattedRNAFile translateToDBNoSequence(RNAFile rnaFile) {
+        if (rnaFile.formatType() != RNAFormatType.BPSEQ)
+            throw new IllegalArgumentException();
+        // add '# before each line of the header'
+        var header = new ArrayList<>(rnaFile.header().stream().map(l -> "#" + l).toList());
+        var body = new ArrayList<String>();
+        // fill the body
+        body.add(rnaFile.structure().getStructure());
+        // create and return the formatted file as object
+        return new FormattedRNAFile(header,body);
     }
 
     @Override
-    public String translateToBPSEQ(RNASecondaryStructure rnaSecondaryStructure) {
+    public FormattedRNAFile translateToBPSEQ(RNAFile rnaFile) {
         return null;
     }
 
     @Override
-    public String translateToCT(RNASecondaryStructure rnaSecondaryStructure) {
+    public FormattedRNAFile translateToCT(RNAFile rnaFile) {
         return null;
     }
 
     @Override
-    public String translateToAAS(RNASecondaryStructure rnaSecondaryStructure) {
+    public FormattedRNAFile translateToAAS(RNAFile rnaFile) {
         return null;
     }
 
     @Override
-    public String translateToAASNoSequence(RNASecondaryStructure rnaSecondaryStructure) {
+    public FormattedRNAFile translateToAASNoSequence(RNAFile rnaFile) {
         return null;
     }
 
     @Override
-    public String translateToFASTA(RNASecondaryStructure rnaSecondaryStructure) {
+    public FormattedRNAFile translateToFASTA(RNAFile rnaFile) {
         return null;
     }
 }
