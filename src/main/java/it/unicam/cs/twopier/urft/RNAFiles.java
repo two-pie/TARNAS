cba2e5121ae30a0e005691fb1d76e676a7db6971
Piermuz7 <piermichele.rosati@gmail.com>
2022-08-30 16:29:42 +0200

    working on RNAFiles utility methods

--- src/main/java/it/unicam/cs/twopier/urft/RNAFiles.java
+++ src/main/java/it/unicam/cs/twopier/urft/RNAFiles.java
@@ -9,17 +9,34 @@ public class RNAFiles {
     }
 
     static List<String> createDBNoSequenceBody(RNASecondaryStructure rnaSecondaryStructure) {
+        // read db no sequence structure and obtain only structure
+        return List.of(getDBBodyOf(rnaSecondaryStructure.size, rnaSecondaryStructure.p));
+    }
+
+    private static String getDBBodyOf(int size, int[] p) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".".repeat(size - 1));
+        for (int i = 1; i <= size; i++)
+            if (p[i] != 0 && sb.charAt(i) == '.' && sb.charAt(p[i]) == '.') {
+                sb.setCharAt(i, '(');
+                sb.setCharAt(p[i], ')');
+            }
+        return sb.toString();
     }
 
     static List<String> createBPSEQBody(RNASecondaryStructure rnaSecondaryStructure) {
+
     }
 
     static List<String> createCTBody(RNASecondaryStructure rnaSecondaryStructure) {
     }
+
     static List<String> createAADBody(RNASecondaryStructure rnaSecondaryStructure) {
     }
+
     static List<String> createAASNoSequenceBody(RNASecondaryStructure rnaSecondaryStructure) {
     }
+
     static List<String> createFASTABody(RNASecondaryStructure rnaSecondaryStructure) {
     }
 }
