368fedb2b1d1a4e9d3aabb4cd999ea9a3cc95af7
Piero <piero.hierro1@gmail.com>
2022-09-20 17:14:23 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.antlr.antlr4.runtime;
-
-    opens it.unicam.cs.twopie.tarnas.view to javafx.fxml;
-    exports it.unicam.cs.twopie.tarnas.view;
-
-}
