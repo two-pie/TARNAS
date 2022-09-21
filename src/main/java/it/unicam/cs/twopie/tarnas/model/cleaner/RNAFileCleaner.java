5f59bcd97630ea2d009cc31094f42fc815c90af4
Piermuz7 <piermichele.rosati@gmail.com>
2022-09-21 15:03:39 +0100

    fixed from record methods to class methods of RNAFile

--- src/main/java/it/unicam/cs/twopie/tarnas/model/cleaner/RNAFileCleaner.java
+++ src/main/java/it/unicam/cs/twopie/tarnas/model/cleaner/RNAFileCleaner.java
@@ -11,21 +11,21 @@ import java.util.function.Predicate;
 public class RNAFileCleaner {
 
     public List<String> mergeDBLines(RNAFile rnaFile) {
-        return rnaFile.format() == RNAFormat.DB ?
+        return rnaFile.getFormat() == RNAFormat.DB ?
                 RNAFileTranslator.translateToDB(rnaFile).body() :
                 RNAFileTranslator.translateToDBNoSequence(rnaFile).body();
     }
 
     public List<String> removeLinesStartingWith(RNAFile rnaFile, char symbol) {
-        return removeIf(rnaFile.header(), line -> !line.startsWith(Character.toString(symbol)));
+        return removeIf(rnaFile.getHeader(), line -> !line.startsWith(Character.toString(symbol)));
     }
 
     public List<String> removeLinesContaining(RNAFile rnaFile, String word) {
-        return removeIf(rnaFile.header(), line -> !line.contains(word));
+        return removeIf(rnaFile.getHeader(), line -> !line.contains(word));
     }
 
     public List<String> removeWhiteSpaces(RNAFile rnaFile) {
-        return removeIf(rnaFile.header(), line -> !line.isBlank());
+        return removeIf(rnaFile.getHeader(), line -> !line.isBlank());
     }
 
     private List<String> removeIf(List<String> header, Predicate<String> predicate) {
