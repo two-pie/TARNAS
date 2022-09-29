8e69e9f9b487b4d1dc662f6303c8164a35e995b7
Piero <68665714+xPieroxk@users.noreply.github.com>
2022-09-29 19:05:17 +0200

    moved files

--- src/main/java/it/unicam/cs/twopie/tarnas/controller/RNAFormatTranslationException.java
+++
@@ -1,8 +0,0 @@
-package it.unicam.cs.twopie.tarnas.controller;
-
-public class RNAFormatTranslationException extends Exception{
-
-    public RNAFormatTranslationException(String message) {
-        super(message);
-    }
-}
