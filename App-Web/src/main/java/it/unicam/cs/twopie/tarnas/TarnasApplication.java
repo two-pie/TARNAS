799913f2ae19cea2b67f45bb1b8a63ba78920a37
Piero <68665714+xPieroxk@users.noreply.github.com>
2022-09-29 19:21:03 +0200

    changed name

---
+++ App-Web/src/main/java/it/unicam/cs/twopie/tarnas/TarnasApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.twopie.tarnas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarnasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarnasApplication.class, args);
+	}
+
+}
---
+++ App-Web/src/test/java/it/unicam/cs/twopie/tarnas/TarnasApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.twopie.tarnas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarnasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
