deafdfed570f7d20a7dc43c41c8cbfd39b28bc32
Piero <68665714+xPieroxk@users.noreply.github.com>
2022-10-08 14:11:59 +0200

    refactoring import

--- Web-App/angular/tarnas/src/app/app-component/app.module.ts
+++ Web-App/angular/tarnas/src/app/app-component/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { TranslateService } from '../service/translate.service';
 import { CleanService } from '../service/clean.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [TranslateService,CleanService],
+  providers: [TranslateService, CleanService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
